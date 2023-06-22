fbe58e640ff417474928ba29a7523e1a318cbd3a
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2023-06-22 03:54:03 +0200

    Format lines + ajout de donnée au getSingleEvent

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,63 +1,94 @@
-const appDb = require('./../server')
+const appDb = require("./../server");
 
 exports.getAllEvents = async (req, res) => {
-    const sql = 'SELECT * FROM event'
+  const sql = "SELECT * FROM event";
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
 
 exports.getAllLightEvents = async (req, res) => {
-    const sql = 'SELECT id, urlImage, libelle FROM event'
+  const sql = "SELECT id, urlImage, libelle FROM event";
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
 
 exports.getAllTrendemousLightEvents = async (req, res) => {
-    const sql = `SELECT id, urlImage, libelle 
+  const sql = `SELECT id, urlImage, libelle 
     FROM event 
-    WHERE isTrendemous = true`
+    WHERE isTrendemous = true`;
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
 
 exports.getSingleEventFromId = async (req, res) => {
-    const sql = `SELECT * 
-    FROM event
-    WHERE id = '${req.params.id}'`
+  const sql = `SELECT e.*, t.id AS ticket_id, t.addressContract, t.prix, t.type, t.date, t.solded
+    FROM event e JOIN ticket t ON e.id = t.idEvent
+    WHERE e.id = '${req.params.id}'
+    AND t.solded = false`;
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      if (result.length === 0) {
+        res.status(404).send({ error: "Element introuvable" });
+      } else {
+        const eventResponse = {
+          id: result[0].id,
+          libelle: result[0].libelle,
+          idCity: result[0].idCity,
+          timestampStart: result[0].timestampStart,
+          timestampEnd: result[0].timestampEnd,
+          idOrganizer: result[0].idOrganizer,
+          timestampCreation: result[0].timestampCreation,
+          isTrendemous: result[0].isTrendemous,
+          urlImage: result[0].urlImage,
+          tickets: [],
+        };
+        console.log(eventResponse)
+
+        result.forEach((row) => {
+            const ticket = {
+                id: row.ticket_id,
+                addressContract: row.addressContract,
+                prix: row.prix,
+                type: row.type,
+                date: row.date,
+                solded: row.solded
+            }
+
+            eventResponse.tickets.push(ticket)
+        })
+        res.status(200).send(eventResponse);
+      }
+    }
+  });
+};
 
 exports.getAllLightEventsFromCat = async (req, res) => {
-    const sql = `SELECT id, urlImage, libelle 
+  const sql = `SELECT id, urlImage, libelle 
     FROM event
     WHERE id = (
         SELECT idEvent
@@ -65,29 +96,29 @@ exports.getAllLightEventsFromCat = async (req, res) => {
         WHERE idCategory = (
             SELECT id
             FROM category
-            WHERE libelle = '${req.params.libelle}'))`
+            WHERE libelle = '${req.params.libelle}'))`;
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
 
 exports.getAllLightEventsFromCity = async (req, res) => {
-    const sql = `SELECT id, urlImage, libelle 
+  const sql = `SELECT id, urlImage, libelle 
     FROM event
-    WHERE idCity = '${req.params.id}'`
+    WHERE idCity = '${req.params.id}'`;
 
-    appDb.db.query(sql, (err, result) => {
-        if (err) {
-            console.log(`Erreur requête : ${err}`)
-            res.status(500).send({ error: 'Error : Internal Server Error' })
-        } else {
-            res.status(200).send(result)
-        }
-    })
-}
+  appDb.db.query(sql, (err, result) => {
+    if (err) {
+      console.log(`Erreur requête : ${err}`);
+      res.status(500).send({ error: "Error : Internal Server Error" });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+};
