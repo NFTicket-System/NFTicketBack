ef763a961bf4a5a312ab8e40b54b28903bc9ed4a
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2023-06-26 16:49:05 +0200

    ajout addEvent

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,94 +1,94 @@
 const appDb = require("./../server");
 
 exports.getAllEvents = async (req, res) => {
-  const sql = "SELECT * FROM event";
+	const sql = "SELECT * FROM event";
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(200).send(result);
+		}
+	});
 };
 
 exports.getAllLightEvents = async (req, res) => {
-  const sql = "SELECT id, urlImage, libelle FROM event";
+	const sql = "SELECT id, urlImage, libelle FROM event";
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(200).send(result);
+		}
+	});
 };
 
 exports.getAllTrendemousLightEvents = async (req, res) => {
-  const sql = `SELECT id, urlImage, libelle 
+	const sql = `SELECT id, urlImage, libelle 
     FROM event 
     WHERE isTrendemous = true`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(200).send(result);
+		}
+	});
 };
 
 exports.getSingleEventFromId = async (req, res) => {
-  const sql = `SELECT e.*, t.id AS ticket_id, t.addressContract, t.prix, t.type, t.date, t.solded
+	const sql = `SELECT e.*, t.id AS ticket_id, t.addressContract, t.prix, t.type, t.date, t.solded
     FROM event e JOIN ticket t ON e.id = t.idEvent
     WHERE e.id = '${req.params.id}'
     AND t.solded = false`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      if (result.length === 0) {
-        res.status(404).send({ error: "Element introuvable" });
-      } else {
-        const eventResponse = {
-          id: result[0].id,
-          libelle: result[0].libelle,
-          timestampStart: result[0].timestampStart,
-          timestampEnd: result[0].timestampEnd,
-          idOrganizer: result[0].idOrganizer,
-          timestampCreation: result[0].timestampCreation,
-          isTrendemous: result[0].isTrendemous,
-          urlImage: result[0].urlImage,
-          city: result[0].city,
-          address: result[0].address,
-          tickets: [],
-        };
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			if (result.length === 0) {
+				res.status(404).send({ error: "Element introuvable" });
+			} else {
+				const eventResponse = {
+					id: result[0].id,
+					libelle: result[0].libelle,
+					timestampStart: result[0].timestampStart,
+					timestampEnd: result[0].timestampEnd,
+					idOrganizer: result[0].idOrganizer,
+					timestampCreation: result[0].timestampCreation,
+					isTrendemous: result[0].isTrendemous,
+					urlImage: result[0].urlImage,
+					city: result[0].city,
+					address: result[0].address,
+					tickets: [],
+				};
 
-        result.forEach((row) => {
-          const ticket = {
-            id: row.ticket_id,
-            addressContract: row.addressContract,
-            prix: row.prix,
-            type: row.type,
-            date: row.date,
-            solded: row.solded,
-          };
+				result.forEach((row) => {
+					const ticket = {
+						id: row.ticket_id,
+						addressContract: row.addressContract,
+						prix: row.prix,
+						type: row.type,
+						date: row.date,
+						solded: row.solded,
+					};
 
-          eventResponse.tickets.push(ticket);
-        });
-        res.status(200).send(eventResponse);
-      }
-    }
-  });
+					eventResponse.tickets.push(ticket);
+				});
+				res.status(200).send(eventResponse);
+			}
+		}
+	});
 };
 
 exports.getAllLightEventsFromCat = async (req, res) => {
-  const sql = `SELECT id, urlImage, libelle 
+	const sql = `SELECT id, urlImage, libelle 
     FROM event
     WHERE id = (
         SELECT idEvent
@@ -98,33 +98,33 @@ exports.getAllLightEventsFromCat = async (req, res) => {
             FROM category
             WHERE libelle = '${req.params.libelle}'))`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(200).send(result);
+		}
+	});
 };
 
 exports.getAllLightEventsFromCity = async (req, res) => {
-  const sql = `SELECT id, urlImage, libelle 
+	const sql = `SELECT id, urlImage, libelle 
     FROM event
     WHERE idCity = '${req.params.id}'`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      res.status(200).send(result);
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(200).send(result);
+		}
+	});
 };
 
 exports.getAllCategoriesEvent = async (req, res) => {
-  const sql = `SELECT *
+	const sql = `SELECT *
     FROM category
     WHERE id IN (
         SELECT idCategory
@@ -132,34 +132,62 @@ exports.getAllCategoriesEvent = async (req, res) => {
         WHERE idEvent = '${req.params.id}'
     )`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      if (result.length === 0) {
-        res.status(404).send({ error: "Element introuvable" });
-      } else {
-        res.status(200).send(result);
-      }
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			if (result.length === 0) {
+				res.status(404).send({ error: "Element introuvable" });
+			} else {
+				res.status(200).send(result);
+			}
+		}
+	});
 };
 
 exports.getAllCategories = async (req, res) => {
-  const sql = `SELECT *
+	const sql = `SELECT *
     FROM category`;
 
-  appDb.db.query(sql, (err, result) => {
-    if (err) {
-      console.log(`Erreur requête : ${err}`);
-      res.status(500).send({ error: "Error : Internal Server Error" });
-    } else {
-      if (result.length === 0) {
-        res.status(404).send({ error: "Element introuvable" });
-      } else {
-        res.status(200).send(result);
-      }
-    }
-  });
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			if (result.length === 0) {
+				res.status(404).send({ error: "Element introuvable" });
+			} else {
+				res.status(200).send(result);
+			}
+		}
+	});
 };
+
+exports.addEvent = async (req, res) => {
+	const libelle = req.body.libelle;
+	const timestampStart = req.body.timestampStart;
+	const timestampEnd = req.body.timestampEnd;
+	const idOrganizer = req.body.idOrganizer;
+	const isTrendemous = req.body.isTrendemous;
+	const urlImage = req.body.urlImage;
+	const city = req.body.city;
+	const address = req.body.address;
+
+	const sql = `INSERT INTO event(libelle, timestampStart, timestampEnd, idOrganizer, isTrendemous, urlImage, city, address)
+    VALUES ('${libelle}', '${timestampStart}', '${timestampEnd}', '${idOrganizer}', '${isTrendemous}', '${urlImage}', '${city}', '${address}')`;
+
+	// Insert new event
+	appDb.db.query(sql, (err, result) => {
+		if (err) {
+			console.log(`Erreur requête : ${err}`);
+			res.status(500).send({ error: "Error : Internal Server Error" });
+		} else {
+			res.status(201).send({ mesage: "Event created" });
+		}
+	});
+};
+
+exports.addTicket = async () => {
+
+} 
